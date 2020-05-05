c940ecbc7e858d9f5d88acd00c0eed03a7ff4772
Shubham Verlekar <shubhamverlekar@ymail.com>
2020-05-05 17:18:31 +0530

    Initial commit

---
+++ application/config.js
@@ -0,0 +1,22 @@
+
+
+
+var config = {
+
+  //Application Title
+  title : "Shubham Youtube Downloader",
+
+  //Error Page Title
+  errorTitle : "Page not Found (404) - GetVideo",
+
+  //Application URL
+  url : "http://yt.shubham.wtf",
+
+  //Application Description
+  description : "Free online download YouTube videos, audio quickly in 1080p, 720p, MP4, 3GP, and more.",
+
+  //Footer text
+  footerText : "Shubham youtube downloader &copy; 2020"
+};
+
+module.exports = config;
